bafdeadfd4f8dc893eeba11e93f09cd46a81a3ce
Mahesh Pantangi <mkpantangi@gmail.com>
2020-12-30 15:14:08 -0500

    Opening  MERN full stack application link

--- src/Assets/DataFiles/ApplicationsData.js
+++ src/Assets/DataFiles/ApplicationsData.js
@@ -2,39 +2,40 @@ const ApplicationsData=[
     {
         id: 1,
         hidden: false,
+        title:"Product Reviews (MERN Stack)",
+        description:"To demonstrate a sample of Full stack (MERN) application.",
+        technology: "MongoDB, Express JS, React JS, Node JS, MERN",        
+        url:"http://localhost:8000/",
+        prd_url:"/product-reviews/"
+    },
+    {
+        id: 2,
+        hidden: false,
         title:"Counter",
         // description:"Simple demonstration of <b>Higher Order Components</b>.<br><br>Code re-usability, two different components executing similar logic based on different events.",
         description:"This application demonstrates implementaion of reusable code when there is a common functional logic among multiple components. In our use case we need to build two components having functionality to increment counter based on different events. Instead of writing incrementor function once per component (twice in total), we would use a higher order component with a function to increment counter and wrap the components with HOC.<br><br>Component #1 would increment count by 1 on \"onClick\" event of a button.<br><br>Component #2 would increment count by 3 on \"onMouseOver\" event of the component.<br><br>Component #3 is the higher order component containing the code to increment the counter.",
         technology: "ReactJS, UseState",
         tags: "HOC (Higher Order Components), State Management",        
         url:"http://localhost:7001",
-        prd_url:"/Counter/"
+        prd_url:"/counter/"
     },
     {
-        id: 2,
+        id: 3,
         hidden: false,
         title:"Appointments",
         description:"To demonstarte state management in simplest form.",
         technology: "MongoDB, Express JS, React, Node JS",      
         url:"http://localhost:7002",  
-        prd_url:"/Appointments/"        
+        prd_url:"/appointments/"        
     },
     {
-        id: 3,
+        id: 4,
         hidden: true,
         title:"Restful API",
         description:"To demonstrate Restful API.",
         technology: "MongoDB, Express JS, React, Node JS, AWS, LAMBDA, Restful API",        
         url:"http://localhost/restful-api/"        
-    },
-    {
-        id: 4,
-        hidden: true,
-        title:"MERN Stack",
-        description:"Ex ipsum aute nulla amet sint et eiusmod fugiat non esse nisi excepteur voluptate deserunt. Deserunt anim irure pariatur consequat qui laboris aliqua voluptate cupidatat pariatur adipisicing dolor in mollit. Minim tempor anim consectetur in aliqua ex elit qui minim ut est. Irure tempor aliqua eiusmod esse culpa enim velit sit commodo qui velit. Ut eiusmod sint sit pariatur eiusmod labore. Fugiat ad irure aliqua eiusmod reprehenderit.",
-        technology: "MongoDB, Express JS, React, Node JS",        
-        url:"http://localhost:7004/"        
-    },
+    },    
     {
         id: 5,
         hidden: true,
